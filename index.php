9f53b2ee41380cf8f62cb5fc0a8826fb3d23002d
maslukasz <lukesekmc@gmail.com>
2024-05-06 20:54:47 +0200

    all tasks from vt s4 benches

--- index.php
+++ index.php
@@ -6,6 +6,15 @@ $onew5ts = '';
 $multiclick = '';
 $anglestrafe = '';
 $smoothbot = '';
+$preciseorb = '';
+$plaza = '';
+$air = '';
+$patstrafe = '';
+$airstrafe = '';
+$psalmts = '';
+$skyts = '';
+$evats = '';
+$bouncets = '';
 
 //  VT Pasu Rasp Intermediate
 if ($_GET['rasp'] >= 750 && $_GET['rasp'] < 850 ) {
@@ -73,7 +82,104 @@ if ($_GET['smoothbot'] >= 3050 && $_GET['smoothbot'] < 3450) {
   $smoothbot = 'Master';
 }
 
+//  VT PreciseOrb Intermediate
+if ($_GET['preciseorb'] >= 1650 && $_GET['preciseorb'] < 2050) {
+  $preciseorb = 'Platinum';
+} elseif ($_GET['preciseorb'] >= 2050 && $_GET['preciseorb'] < 2450) {
+  $preciseorb = 'Diamond';
+} elseif ($_GET['preciseorb'] >= 2450 && $_GET['preciseorb'] < 2850) {
+  $preciseorb = 'Jade';
+} elseif ($_GET['preciseorb'] >= 2850) {
+  $preciseorb = 'Master';
+}
 
+//  VT Plaza Intermediate
+if ($_GET['plaza'] >= 2680 && $_GET['plaza'] < 2980) {
+  $plaza = 'Platinum';
+} elseif ($_GET['plaza'] >= 2980 && $_GET['plaza'] < 3280) {
+  $plaza = 'Diamond';
+} elseif ($_GET['plaza'] >= 3280 && $_GET['plaza'] < 3530) {
+  $plaza = 'Jade';
+} elseif ($_GET['plaza'] >= 3530) {
+  $plaza = 'Master';
+}
+
+//  VT Air Intermediate
+if ($_GET['air'] >= 2450 && $_GET['air'] < 2700) {
+  $air = 'Platinum';
+} elseif ($_GET['air'] >= 2700 && $_GET['air'] < 2950) {
+  $air = 'Diamond';
+} elseif ($_GET['air'] >= 2950 && $_GET['air'] < 3200) {
+  $air = 'Jade';
+} elseif ($_GET['air'] >= 3200) {
+  $air = 'Master';
+}
+
+//  VT Air Intermediate
+if ($_GET['patstrafe'] >= 2260 && $_GET['patstrafe'] < 2620) {
+  $patstrafe = 'Platinum';
+} elseif ($_GET['patstrafe'] >= 2620 && $_GET['patstrafe'] < 2800) {
+  $patstrafe = 'Diamond';
+} elseif ($_GET['patstrafe'] >= 2800 && $_GET['patstrafe'] < 3050) {
+  $patstrafe = 'Jade';
+} elseif ($_GET['patstrafe'] >= 3050) {
+  $patstrafe = 'Master';
+}
+
+//  VT AirStrafe Intermediate
+if ($_GET['airstrafe'] >= 2800 && $_GET['airstrafe'] < 3000) {
+  $airstrafe = 'Platinum';
+} elseif ($_GET['airstrafe'] >= 3000 && $_GET['airstrafe'] < 3200) {
+  $airstrafe = 'Diamond';
+} elseif ($_GET['airstrafe'] >= 3200 && $_GET['airstrafe'] < 3400) {
+  $airstrafe = 'Jade';
+} elseif ($_GET['airstrafe'] >= 3400) {
+  $airstrafe = 'Master';
+}
+
+//  VT psalmTS Intermediate
+if ($_GET['psalmts'] >= 810 && $_GET['psalmts'] < 880) {
+  $psalmts = 'Platinum';
+} elseif ($_GET['psalmts'] >= 880 && $_GET['psalmts'] < 950) {
+  $psalmts = 'Diamond';
+} elseif ($_GET['psalmts'] >= 950 && $_GET['psalmts'] < 1020) {
+  $psalmts = 'Jade';
+} elseif ($_GET['psalmts'] >= 1020) {
+  $psalmts = 'Master';
+}
+
+//  VT skyTS Intermediate
+if ($_GET['skyts'] >= 1030 && $_GET['skyts'] < 1130) {
+  $skyts = 'Platinum';
+} elseif ($_GET['skyts'] >= 1130 && $_GET['skyts'] < 1220) {
+  $skyts = 'Diamond';
+} elseif ($_GET['skyts'] >= 1220 && $_GET['skyts'] < 1300) {
+  $skyts = 'Jade';
+} elseif ($_GET['skyts'] >= 1300) {
+  $skyts = 'Master';
+}
+
+//  VT evaTS Intermediate
+if ($_GET['evats'] >= 550 && $_GET['evats'] < 650) {
+  $evats = 'Platinum';
+} elseif ($_GET['evats'] >= 600 && $_GET['evats'] < 650) {
+  $evats = 'Diamond';
+} elseif ($_GET['evats'] >= 650 && $_GET['evats'] < 700) {
+  $evats = 'Jade';
+} elseif ($_GET['evats'] >= 700) {
+  $evats = 'Master';
+}
+
+//  VT bounceTS Intermediate
+if ($_GET['bouncets'] >= 630 && $_GET['bouncets'] < 650) {
+  $bouncets = 'Platinum';
+} elseif ($_GET['bouncets'] >= 670 && $_GET['bouncets'] < 650) {
+  $bouncets = 'Diamond';
+} elseif ($_GET['bouncets'] >= 710 && $_GET['bouncets'] < 760) {
+  $bouncets = 'Jade';
+} elseif ($_GET['bouncets'] >= 760) {
+  $bouncets = 'Master';
+}
 ?>
 
 <head>
@@ -121,9 +227,54 @@ if ($_GET['smoothbot'] >= 3050 && $_GET['smoothbot'] < 3450) {
         <td><?= $arcstrafe ?></td>
       </tr>
       <tr>
-        <td class='bg-red-500'> VT Smoothbot Intermediate</td>
+        <td class='bg-green-500'> VT Smoothbot Intermediate</td>
         <td><input type="text" name="smoothbot" form="my_form" /></td>
         <td><?= $smoothbot ?></td>
+      </tr>
+      <tr>
+        <td class='bg-green-500'> VT PreciseOrb Intermediate</td>
+        <td><input type="text" name="preciseorb" form="my_form" /></td>
+        <td><?= $preciseorb ?></td>
+      </tr>
+      <tr>
+        <td class='bg-blue-500'> VT Plaza Intermediate</td>
+        <td><input type="text" name="plaza" form="my_form" /></td>
+        <td><?= $plaza ?></td>
+      </tr>
+      <tr>
+        <td class='bg-blue-500'> VT Air Intermediate</td>
+        <td><input type="text" name="air" form="my_form" /></td>
+        <td><?= $air ?></td>
+      </tr>
+      <tr>
+        <td class='bg-pink-500'> VT PatStrafe Intermediate</td>
+        <td><input type="text" name="patstrafe" form="my_form" /></td>
+        <td><?= $patstrafe ?></td>
+      </tr>
+      <tr>
+        <td class='bg-pink-500'> VT AirStrafe Intermediate</td>
+        <td><input type="text" name="airstrafe" form="my_form" /></td>
+        <td><?= $airstrafe ?></td>
+      </tr>
+      <tr>
+        <td class='bg-red-500'> VT psalmTS Intermediate</td>
+        <td><input type="text" name="psalmts" form="my_form" /></td>
+        <td><?= $psalmts ?></td>
+      </tr>
+      <tr>
+        <td class='bg-red-500'> VT skyTS Intermediate</td>
+        <td><input type="text" name="skyts" form="my_form" /></td>
+        <td><?= $skyts ?></td>
+      </tr>
+      <tr>
+        <td class='bg-purple-700'> VT evaTS Intermediate</td>
+        <td><input type="text" name="evats" form="my_form" /></td>
+        <td><?= $evats ?></td>
+      </tr>
+      <tr>
+        <td class='bg-purple-700'> VT bounceTS Intermediate</td>
+        <td><input type="text" name="bouncets" form="my_form" /></td>
+        <td><?= $bouncets ?></td>
       </tr>
     </table>
     <button type="submit" form="my_form">ok</button>
